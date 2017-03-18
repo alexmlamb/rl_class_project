912c8a0d701df25e106be282fd39f1865539e28d
Alexander Lamb <lambalex@leto05.iro.umontreal.ca>
2017-03-18 19:37:29 -0400

    updates

--- lib/critic.py
+++ lib/critic.py
@@ -1,10 +1,14 @@
 
+def init_params(p):
+
+    return p
+
 '''
     probs is (time x example x token)
     rewards is (example)
 
     Network goes over probs and estimates final reward using an RNN.  
 '''
-def network(probs,rewards):
+def network(probs,rewards, params):
 
 
